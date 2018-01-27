9ebcc987ff724c52585ef8796ab496b4e690a305
Bartosz Kluchciński <bkluchcinski@gmail.com>
2018-01-28 00:37:27 +0100

    added menu

--- Assets/Scripts/UI/PlayButtonClick.cs
+++ Assets/Scripts/UI/PlayButtonClick.cs
@@ -5,10 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class PlayButtonClick : MonoBehaviour
 {
-    public Scene scene;
+    public string SceneName;
+
+    public GameObject Levels;
+
+    private bool isMovingLeft, isMovingRight;
+
+    private Vector3 target;
+
+    public void OnPlayLevelClick()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    private void Update()
+    {
+        if (isMovingLeft)
+        {
+            Levels.GetComponent<RectTransform>().anchoredPosition = Vector3.MoveTowards(Levels.GetComponent<RectTransform>().anchoredPosition, target, 100);
+            if (Levels.GetComponent<RectTransform>().anchoredPosition.x <= target.x)
+            {
+                isMovingLeft = false;
+                Levels.GetComponent<RectTransform>().anchoredPosition = target;
+            }
+        }
+
+        if (isMovingRight)
+        {
+            Levels.GetComponent<RectTransform>().anchoredPosition = Vector3.MoveTowards(Levels.GetComponent<RectTransform>().anchoredPosition, target, 100);
+            if (Levels.GetComponent<RectTransform>().anchoredPosition.x >= target.x)
+            {
+                isMovingRight = false;
+                Levels.GetComponent<RectTransform>().anchoredPosition = target;
+            }
+        }
+    }
 
     public void OnPlayClick()
     {
-        SceneManager.LoadScene(scene.name);
+        if (!isMovingRight)
+        {
+            isMovingLeft = true;
+            target = new Vector3(-1920, 0, 0);
+        }
+    }
+
+    public void OnMenuBackClick()
+    {
+        if (!isMovingRight)
+        {
+            isMovingRight = true;
+            target = new Vector3(0, 0, 0);
+        }
     }
 }
