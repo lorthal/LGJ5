0e8fdbdbce1cc63a7af6d80120ecb9d1f5d03c32
Bartosz Kluchciński <bkluchcinski@gmail.com>
2018-01-28 02:22:39 +0100

    added lantern icon on ui

---
+++ Assets/OldAdroidFix.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class OldAdroidFix : MonoBehaviour
+{
+
+    public Material OldAndroidMaterial;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private void Awake()
+    {
+        if(GetSDKLevel() < 19)
+        {
+            GetComponent<Renderer>().sharedMaterial = OldAndroidMaterial;
+        }
+    }
+
+    public int GetSDKLevel()
+    {
+        var clazz = AndroidJNI.FindClass("android.os.Build$VERSION");
+        var fieldID = AndroidJNI.GetStaticFieldID(clazz, "SDK_INT", "I");
+        var sdkLevel = AndroidJNI.GetStaticIntField(clazz, fieldID);
+        return sdkLevel;
+    }
+#endif
+}
---
+++ Assets/Scripts/UI/LanternUIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanternUIController : MonoBehaviour
+{
+
+    public Text lanternCount;
+	void Update ()
+	{
+	    lanternCount.text = "" + (LanternsManager.Instance.maxLanternCount - LanternsManager.Instance.lanters.Count);
+	}
+}
