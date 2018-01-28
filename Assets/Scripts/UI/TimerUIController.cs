36e5c511b59a60c1e3b8f0f9e4ebdc8340366448
Bartosz Kluchciński <bkluchcinski@gmail.com>
2018-01-28 03:17:47 +0100

    added stars on timer

--- Assets/Scripts/UI/TimerUIController.cs
+++ Assets/Scripts/UI/TimerUIController.cs
@@ -9,10 +9,30 @@ public class TimerUIController : MonoBehaviour
     public GameObject Pointer;
     public Image FillImage;
 
+    public GameObject Star3;
+    public GameObject Star2;
+
+    public Sprite LostStarSprite;
+
+    private void Start()
+    {
+        Star3.transform.localRotation = Quaternion.AngleAxis(-360.0f * (1 - ReciverManager.Instance.Star3Percent), Vector3.forward);
+        Star2.transform.localRotation = Quaternion.AngleAxis(-360.0f * (1 - ReciverManager.Instance.Star2Percent), Vector3.forward);
+    }
 
     private void Update()
     {
         Pointer.transform.localRotation = Quaternion.AngleAxis(-360.0f * (1 - ReciverManager.Instance.GetTimePercentage()), Vector3.forward);
         FillImage.fillAmount = ReciverManager.Instance.GetTimePercentage();
+
+        if (ReciverManager.Instance.StarRecived == 2)
+        {
+            Star3.GetComponentInChildren<Image>().sprite = LostStarSprite;
+        }
+
+        if (ReciverManager.Instance.StarRecived == 1)
+        {
+            Star2.GetComponentInChildren<Image>().sprite = LostStarSprite;
+        }
     }
 }
